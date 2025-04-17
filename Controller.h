798464a2cc42b7dc0101bbfcaad40422032d4ef8
Umairishuman <muhammadumair19925@gmail.com>
2025-04-17 22:26:15 +0500

    flight  fun

--- Controller.h
+++ Controller.h
@@ -6,6 +6,10 @@
 #include "flight.h"
 #include <pthread.h>
 
+#define NORTH 0
+#define SOUTH 1
+#define EAST 2
+#define WEST 3
 class Controller {
     vector<Airline*> airlines;
     vector<Runway*> runways;
@@ -14,6 +18,8 @@ class Controller {
 
     pthread_t flightGeneratorThread;
     pthread_attr_t flightGeneratorAttr;
+
+    pthread_mutex_t lock;
 public:
     Controller() {
         initilaizeAirlines();
@@ -33,6 +39,10 @@ public:
         runways.push_back(new Runway(3, "RWY-C", "All", "Cargo/Emergency/Military"));
 
 
+        //testing purposes
+        // for(auto &runway : runways){
+        //     runway->printRunwayDetails();
+        // }
     } 
     void initilaizeAirlines() {
         airlines.push_back(new Airline(1, "PIA", "Commercial", 6, 4));
@@ -41,22 +51,115 @@ public:
         airlines.push_back(new Airline(4, "Pakistan Air Force", "Military", 2, 1));
         airlines.push_back(new Airline(5, "Blue Dart", "Cargo", 2, 2));
         airlines.push_back(new Airline(6, "Agha Khan Air Ambulance", "Medical", 2, 1));
+
+        //testing purposes
+        // for(auto &airline : airlines){
+        //     airline->print();
+        // }
     }
 
-    static void* flightGenerator(void* arg){
+    // static void* flightGenerator(void* arg){
 
-        // Direction, Flight Type, Time, Emergency Probability
-        // North, International Arrivals, Every 3 minutes, 10% (low fuel/diversion)
-        // South, Domestic Arrivals, Every 2 minutes, 5% (air ambulance)
-        // East, International Departures, Every 2.5 minutes, 15% (military/priority)
-        // West, Domestic Departures, Every 4 minutes, 20% (VIP or urgent cargo)
-        Controller *controller = (Controller*)arg;
+    //     // Direction, Flight Type, Time, Emergency Probability
+    //     // North, International Arrivals, Every 3 minutes, 10% (low fuel/diversion)
+    //     // South, Domestic Arrivals, Every 2 minutes, 5% (air ambulance)
+    //     // East, International Departures, Every 2.5 minutes, 15% (military/priority)
+    //     // West, Domestic Departures, Every 4 minutes, 20% (VIP or urgent cargo)
+    //     Controller *controller = (Controller*)arg;
 
 
-        cout << "Generating flights..." << endl;
+    //     cout << "Generating flights..." << endl;
 
-        return nullptr;
+    //     return nullptr;
+    // }
+
+    bool generateEmergency(double probabilityPercent) {
+        if(rand() % 100 < probabilityPercent * 100) {
+            return true; // Emergency flight
+        }
+        return false;
     }
+    
+    static void* flightGenerator(void* arg) {
+        Controller* controller = (Controller*)arg;
+        srand(time(NULL));
+        
+        //flight times 
+        const double flightTimes[] = {3.0, 2.0, 2.5, 4.0}; // in minutes
+
+
+        bool cargoHasBeenGenerated = false;
+        double elapsedTime[] = {0.0, 0.0, 0.0, 0.0};
+        double emergencyProbabilities[] = {0.1, 0.5, 0.15, 0.2};
+        
+        // here i am decalaring a variable to hold the arrival intervals
+        //the flight should also use an available aircraft
+        //so the algo goes like this
+        //first i 
+        //
+        int flightId = 0;
+        while (true) {
+            for (int i = 0; i < 4; i++) {
+                elapsedTime[i] += 0.1; // increment elapsed time by 0.1 minutes
+
+                if (elapsedTime[i] == 0.0) {
+                    elapsedTime[i] = flightTimes[i]; // reset elapsed time
+                    bool isEmergency = controller->generateEmergency(emergencyProbabilities[i]);
+                    string flightType;
+                    string direction;
+                    string airlineName;
+                    int priority;
+
+                    switch (i) {
+                        case NORTH:
+                            flightType = "International Arrivals";
+                            direction = "North";
+                            airlineName = controller->airlines[0]->getName();
+                            priority = 1;
+                            break;
+                        case SOUTH:
+                            flightType = "Domestic Arrivals";
+                            direction = "South";
+                            airlineName = controller->airlines[1]->getName();
+                            priority = 2;
+                            break;
+                        case EAST:
+                            flightType = "International Departures";
+                            direction = "East";
+                            airlineName = controller->airlines[2]->getName();
+                            priority = 3;
+                            break;
+                        case WEST:
+                            flightType = "Domestic Departures";
+                            direction = "West";
+                            airlineName = controller->airlines[3]->getName();
+                            priority = 4;
+                            break;
+                    }
+
+                    Flight* newFlight = new Flight(flightId++, flightType, airlineName, direction, priority, nullptr);
+                    if (isEmergency) {
+                        newFlight->setEmergencyFlight(true);
+                    }
+                    
+                    // Add the new flight to the appropriate vector
+                    if (i < 2) { // North and South are arrivals
+                        controller->arrivals.push_back(newFlight);
+                    } else { // East and West are departures
+                        controller->departures.push_back(newFlight);
+                    }
+                    
+                    cout << "Generated Flight: ID=" << newFlight->getId() << ", Type=" << flightType << ", Direction=" << direction << ", Airline=" << airlineName << ", Emergency=" << (isEmergency ? "Yes" : "No") << endl;
+
+                }
+            }
+            usleep(100000); // Sleep for 0.1 seconds
+            
+        }  
+        return  nullptr;
+    }
+    
+    
 };
 
 #endif
--- airline.h
+++ airline.h
@@ -12,21 +12,28 @@ class Airline{
     int totalAircrafts;
     int flights;
     vector<Aircraft*> aircrafts;
+
+    int flightsLeft;
+
     
 
 public:
     Airline(int id, string name, string type, int totalAircrafts, int flights) : id(id), name(name), type(type), totalAircrafts(totalAircrafts), flights(flights) {
-        string firstTwo = name.substr(0, 2);
+        
+        this->flightsLeft = flights;
         
         // constant varibles for assaan readability
-
+        
+        string firstTwo = name.substr(0, 2);
         const int INITIAL_X = 0;
         const int INITIAL_Y = 0;
         const string INITIAL_STATUS = "Grounded";
         const int INITIAL_SPEED = 0;
 
         for(int i = 0; i < totalAircrafts; i++){
-            aircrafts.push_back(new Aircraft(i, firstTwo + to_string(i), type, INITIAL_X, INITIAL_Y, INITIAL_STATUS, INITIAL_SPEED));
+            aircrafts.push_back(new Aircraft(i, firstTwo + "-" +to_string(i), type, INITIAL_X, INITIAL_Y, INITIAL_STATUS, INITIAL_SPEED));
+            aircrafts[i]->setAirline(name);
+            aircrafts[i]->setStatus("Grounded");
         }
     }
 
@@ -64,6 +71,12 @@ public:
         aircrafts.push_back(aircraft);
         totalAircrafts++;
     }
+    void setFlightsLeft(int newFlightsLeft) {
+        flightsLeft = newFlightsLeft;
+    }
+    int getFlightsLeft() const {
+        return flightsLeft;
+    }
     
     void displayAircrafts() const {
         for (const auto& aircraft : aircrafts) {
@@ -71,11 +84,16 @@ public:
         }
     }
     void print() const {
-        std::cout << "Airline ID: " << id << "\n"
-                  << "Name: " << name << "\n"
-                  << "Type: " << type << "\n"
-                  << "Total Aircrafts: " << totalAircrafts << "\n"
-                  << "Flights: " << flights << "\n";
+        cout << "Airline ID: " << id << "\n"
+             << "Name: " << name << "\n"
+             << "Type: " << type << "\n"
+             << "Total Aircrafts: " << totalAircrafts << "\n"
+             << "Flights: " << flights << "\n";
+        cout << "Aircrafts: \n";
+        for (const auto& aircraft : aircrafts) {
+            aircraft->print();
+        }
+        cout << endl;
     }
     void printAircrafts() const {
         for (const auto& aircraft : aircrafts) {
